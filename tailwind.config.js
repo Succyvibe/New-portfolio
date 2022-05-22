faefc3c809e790f07665d4db6f2446eec8f95d0e
success ibekwe <Successibekwe047@gmail.com>
2022-05-22 05:11:34 -0800

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,41 @@
+module.exports = {
+  content: ['./*.html'],
+  theme: {
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px'
+    },
+    extend: {
+      colors: {
+        ContentrBodyText: '#102A27',
+        background: '#E5E5E5',
+        Teal: '#4FD1C5',
+        TealLight: '#DCF6F3',
+        contentrePositive: '#102A27',
+        contentrePositiveLight: '#D8FFE8',
+        darkGreen: '#286963',
+        dark: '#102A27',
+        darkText: '#333333',
+        grey1: '#888888',
+        grey2: '#BDBDBD',
+        grey3: '#99999999',
+        contentreNegative: '#FD4E4E',
+        contentreNegativeLight: '#FFCFCF',
+        greyGreen: '#859492',
+        teallight1: 'EBFAF8',
+        greybrown: '#666666',
+        textDark: '000000',
+        cardBg: '#FCFCFC',
+        teallight2: '#DCEFF0',
+        leavegreen: '#008F98',
+        darkGreen1: '#102A27',
+        textteal: '#B0C8C5',
+        milkyWhite: '#FCFCFC',
+
+      },
+    },
+  },
+  plugins: [],
+}
